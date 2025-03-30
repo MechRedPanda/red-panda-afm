e29fab9dc84b4ae3074c7faaa31c4a2756256bea
MechRedPanda <meowsmodel@gmail.com>
2025-03-30 13:37:59 -0700

    add password config

---
+++ firmware/include/sample_config.hpp
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi credentials
+#define WIFI_SSID "Your_SSID"
+#define WIFI_PASSWORD "Your_PASSWORD"
+
+// Any other configuration variables can go here...
+
+#endif // CONFIG_H
